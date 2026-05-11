8e7d2599575920cdf3542a35c84d641ad8377f63
thanetadisak-hub <thanet.adisak@gmail.com>
2026-05-11 13:53:43 +0700

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/organic-chemistry-website/',
   server: {
     host: true,
-    port: 5173
-  }
+    port: 5173,
+  },
 });
